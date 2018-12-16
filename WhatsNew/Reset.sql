4c34884a55d8894fff643fcd3431b94ae4243c69
Bill Fellows <tenaciousb@gmail.com>
2018-12-16 09:26:47 -0600

    Update WhatsNew/Reset.sql
    Dynamic up in here

--- WhatsNew/Reset.sql
+++ WhatsNew/Reset.sql
@@ -1,4 +1,4 @@
-USE master;
+SET NOCOUNT ON;
 -- drop with extreme prejudice
 IF EXISTS(SELECT * FROM sys.databases AS D WHERE D.name = 'DemoDb')
 BEGIN
@@ -7,24 +7,34 @@ BEGIN
 END
 GO
 DECLARE
-    @logPath sysname = CONVERT(sysname, SERVERPROPERTY('InstanceDefaultLogPath'))
-,   @dataPath sysname = CONVERT(sysname, SERVERPROPERTY('InstanceDefaultDataPath'));
+	@mdfPath varchar(250) = (SELECT TOP 1  REPLACE(DF.physical_name, 'master.mdf', '') FROM sys.master_files AS DF WHERE DF.type = 0 AND DF.file_id = 1 AND database_id = 1)
+,	@pathToken nvarchar(20) = '<DataPath/>'
+,	@cmd nvarchar(max) = ''
+,	@templateCreateDb nvarchar(max) = N'
 
-DECLARE @MakeDemoDb nvarchar(max) = N'
 CREATE DATABASE DemoDb CONTAINMENT = NONE
-ON PRIMARY
-(
-    NAME = N''DemoDb''
-,   FILENAME = N'''+ @dataPath + 'DemoDb.mdf''
-,   SIZE = 8192KB
-,   FILEGROWTH = 65536KB
-)
-LOG ON
-(
-    NAME = N''DemoDb_log''
-,   FILENAME = N'''+ @logPath + 'DemoDb_log.ldf''
-,   SIZE = 8192KB
-,   FILEGROWTH = 65536KB
-);'
+    ON PRIMARY
+           (
+               NAME = N''DemoDb''
+           ,   FILENAME = N''<DataPath/>DemoDb.mdf''
+           ,   SIZE = 8192KB
+           ,   FILEGROWTH = 65536KB
+           )
+    LOG ON
+        (
+            NAME = N''DemoDb_log''
+        ,   FILENAME = N''<DataPath/>DemoDb_log.ldf''
+        ,   SIZE = 8192KB
+        ,   FILEGROWTH = 65536KB
+        );
+';
 
-EXECUTE sys.sp_executesql @MakeDemoDb, N'';
+	SELECT 
+		@cmd = REPLACE(@templateCreateDb, @pathToken, @mdfPath);
+
+	IF 1=1
+	BEGIN
+		PRINT @cmd
+	END	
+	EXECUTE sys.sp_executesql @cmd, N'';
+GO
